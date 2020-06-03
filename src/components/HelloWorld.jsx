645a5f70667d80298fd9de015d77742d2fdef6d6
noe adrian acuña prado <acuscorp@gmail.com>
2020-06-03 01:17:00 -0500

    setup base

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../assets/styles/App.scss';
+/**
+ * @description nota que se devuelve entre parentesis el h1
+ * y no entre corchetes
+ */
+const HelloWorld = () => (
+  <h1>Hola mundo</h1>
+);
+
+export default HelloWorld;
